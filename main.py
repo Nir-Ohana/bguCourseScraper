2949e7560a13b24a5dcd49a261f961dba10378f5
Nir <speednir123@gmail.com>
2021-06-02 00:00:59 +0300

    update

--- main.py
+++ main.py
@@ -19,8 +19,9 @@ def click(x, y):
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, x, y, 0, 0)
 
 
-def scrap_courses(department_number):
+def scrap_courses(department_number, department_name):
     try:
+        this_department_course_list = []
         WebDriverWait(driver, 10).until(
             EC.element_to_be_clickable((By.ID, "BTN_RESET"))
         ).click()
@@ -42,17 +43,20 @@ def scrap_courses(department_number):
             course_number = col[0].text
             open_in = col[1].text
 
-            courses_current_department[course_name] = {
+            this_department_course_list.append({
+                "department_number": department_number,
+                "department_name": department_name,
+                "course_name": course_name,
                 "course_number": course_number,
                 "open_in": open_in
-            }
+            })
 
         driver.find_element_by_class_name('styled-button-2').click()
         department = WebDriverWait(driver, 10).until(
             EC.visibility_of_element_located((By.ID, "on_course_department"))
         )
         department.clear()
-        return courses_current_department
+        return this_department_course_list
 
     except Exception as e:
         print(f"Exception in scrap courses function : {str(e)}")
@@ -64,7 +68,7 @@ def scrap_courses(department_number):
 
 
 if __name__ == '__main__':
-    courses = {}
+    courses = []
     courses_extended = {}
     departments = []
     course_page_template = 'https://bgu4u.bgu.ac.il/pls/scwp/!app.gate?app=ann&lang=he&step=3&st=s&popup=cns' \
@@ -83,20 +87,20 @@ if __name__ == '__main__':
     options = departments_list_selector.options
     for index in range(0, len(options) - 1):
         department_number_name_text = str(options[index].text).split(' - ')
-        departments.append(department_number_name_text[0])
+        if len(department_number_name_text) == 2:
+            departments.append((department_number_name_text[0], department_number_name_text[1]))
 
     try:
-        for department_number in departments:
+        for department_number_name in departments:
             try:
+                department_number = department_number_name[0]
+                department_name = department_number_name[1]
                 if department_number.startswith('0'):
                     department_number = department_number.lstrip('0')
-                result = scrap_courses(department_number)
+                result = scrap_courses(department_number, department_name)
                 if result is not None:
-                    courses.update(
-                        {
-                            department_number: result
-                        }
-                    )
+                    for item in result:
+                        courses.append(item)
                 time.sleep(2)
             except Exception as e:
                 print(f"Exception in main : {str(e)}")
@@ -106,60 +110,60 @@ if __name__ == '__main__':
                 department.clear()
                 continue
 
-        for department in list(courses.keys()):
-            try:
-                print(department)
-                courses_extended[department] = {}
-                for course in list(courses[department].keys()):
-                    print(course)
-                    _, course_degree_level, course_number = courses[department][course]['course_number'].split('.')
-                    year, semester_number = courses[department][course]['open_in'].split('-')
-                    url = course_page_template.format(department=department,
-                                                      course_degree_level=course_degree_level,
-                                                      course_number=course_number,
-                                                      year=year, semester_number=semester_number)
-                    driver.get(url)
-                    frame = driver.find_element_by_name('main')
-                    driver.switch_to.frame(frame)
-                    course_unordered_list = driver.find_elements_by_tag_name('li')
-
-                    credit_points = str(course_unordered_list[4].text).split(':')[1].rstrip('.00')
-                    test = str(course_unordered_list[12].text)
-                    li_list = driver.find_elements_by_class_name('BlackInput')
-
-                    time = ''
-                    time_re = re.compile('(([01][0-9]|2[0-3]):([0-5][0-9]))')
-                    match_time = time_re.findall(str(li_list[3].text))
-                    if match_time is not None and len(match_time) == 2:
-                        time = match_time[0][0] + '-' + match_time[1][0]
-
-                    day = ''
-                    day_time = ''
-                    day_re = re.compile('(יום [א|ב|ג|ד|ה|ו])')
-                    match_day = day_re.findall(str(li_list[3].text))
-                    if len(match_day) > 0:
-                        day_time = match_day[0] + ' ' + time
-
-                    print(day_time)
-                    courses_extended[department][course] = {
-                        "course_number": courses[department][course]['course_number'],
-                        "open_in": courses[department][course]['open_in'],
-                        "credit_points": credit_points,
-                        "test_exist": not (CONST_IS_EXAM in test),
-                        "professor name": li_list[2].text,
-                        "time": day_time
-                    }
-
-                    print('\n')
-                    print('_' * 69)
-
-            except Exception as e:
-                print(f"Exception in scraping specific course in department : {department}\n{str(e)}")
-                department = WebDriverWait(driver, 10).until(
-                    EC.visibility_of_element_located((By.ID, "on_course_department"))
-                )
-                department.clear()
-                continue
+        # for department in list(courses.keys()):
+        #     try:
+        #         print(department)
+        #         courses_extended[department] = {}
+        #         for course in list(courses[department].keys()):
+        #             print(course)
+        #             _, course_degree_level, course_number = courses[department][course]['course_number'].split('.')
+        #             year, semester_number = courses[department][course]['open_in'].split('-')
+        #             url = course_page_template.format(department=department,
+        #                                               course_degree_level=course_degree_level,
+        #                                               course_number=course_number,
+        #                                               year=year, semester_number=semester_number)
+        #             driver.get(url)
+        #             frame = driver.find_element_by_name('main')
+        #             driver.switch_to.frame(frame)
+        #             course_unordered_list = driver.find_elements_by_tag_name('li')
+        #
+        #             credit_points = str(course_unordered_list[4].text).split(':')[1].rstrip('.00')
+        #             test = str(course_unordered_list[12].text)
+        #             li_list = driver.find_elements_by_class_name('BlackInput')
+        #
+        #             time = ''
+        #             time_re = re.compile('(([01][0-9]|2[0-3]):([0-5][0-9]))')
+        #             match_time = time_re.findall(str(li_list[3].text))
+        #             if match_time is not None and len(match_time) == 2:
+        #                 time = match_time[0][0] + '-' + match_time[1][0]
+        #
+        #             day = ''
+        #             day_time = ''
+        #             day_re = re.compile('(יום [א|ב|ג|ד|ה|ו])')
+        #             match_day = day_re.findall(str(li_list[3].text))
+        #             if len(match_day) > 0:
+        #                 day_time = match_day[0] + ' ' + time
+        #
+        #             print(day_time)
+        #             courses_extended[department][course] = {
+        #                 "course_number": courses[department][course]['course_number'],
+        #                 # "open_in": courses[department][course]['open_in'],
+        #                 "credit_points": credit_points,
+        #                 "test_exist": not (CONST_IS_EXAM in test),
+        #                 "professor name": li_list[2].text
+        #                 # "time": day_time
+        #             }
+        #
+        #             print('\n')
+        #             print('_' * 69)
+        #
+        #     except Exception as e:
+        #         print(f"Exception in scraping specific course in department : {department}\n{str(e)}")
+        #         department = WebDriverWait(driver, 10).until(
+        #             EC.visibility_of_element_located((By.ID, "on_course_department"))
+        #         )
+        #         department.clear()
+        #         continue
 
 
     finally:
